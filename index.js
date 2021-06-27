e0e25b7508a02ed02d12ae11c6bd08e091c0daf6
TrevorZylks <trevorzylks@gmail.com>
2021-06-27 08:19:27 -0700

    commit tree a27323ad801a82debe7d1595a4f15c6eb1dab01f

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber <= 400) {
+    result = "This one is on me!";
+  } else if (someNumber > 2000 && someNumber < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tips){
+  switch (tips) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+    return 'Bye.';
+    break;
+  }
+  } 
